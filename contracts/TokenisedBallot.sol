2f382c6d321d5568a830d7b7b85f8800b6604fb3
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-08-26 11:57:48 +0100

    implement voting

--- contracts/TokenisedBallot.sol
+++ contracts/TokenisedBallot.sol
@@ -14,19 +14,31 @@ contract  TokenisedBallot {
     IMyToken public tokenContract;
     Proposal[] public proposals;
     uint256 public targetBlockNumber;
+    mapping(address => uint256) public spentVotePower;
 
     constructor(bytes32[] memory _proposalNames, address _tokenContract,
         uint256 _targetBlockNumber) {
         tokenContract = IMyToken(_tokenContract);
         targetBlockNumber = _targetBlockNumber;
         // TODO: Validate if targetBlockNumber is in the past
+        //  ERC20Votes lets you keep track of historical voting power
         for (uint i = 0; i < _proposalNames.length; i++) {
             proposals.push(Proposal({name: _proposalNames[i], voteCount: 0}));
         }
     }
 
     function vote(uint256 proposal, uint256 amount) external {
-        // TODO: Implement vote function
+        uint256 senderVotingPower = getVoterPower(msg.sender); // TODO
+        // we should be concerned with voting power and not balanced
+        // yet care for voting power and self delegating
+        require(senderVotingPower >= amount, "TokenizedBallot: insufficient voting power");
+       // do first before voting
+        spentVotePower[msg.sender] += amount;
+        proposals[proposal].voteCount += amount;
+    }
+
+    function getVoterPower(address voter) public view returns (uint256 votePower_) {
+        votePower_ = tokenContract.getPastVotes(voter, targetBlockNumber) - spentVotePower[voter];
     }
 
     function winningProposal() public view returns (uint winningProposal_) {
