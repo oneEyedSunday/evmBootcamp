10866681d1c6bdf9b7e017276f2b2b763c8bd01f
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-09-06 02:10:56 +0100

    setup Swagger docs

--- week4/backend/src/main.ts
+++ week4/backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+
+function setupApiDocs(_app: INestApplication<any>) {
+  const config = new DocumentBuilder()
+    .setTitle('Voting dApp API')
+    .setDescription(
+      'The API exposes a voting dApp to cast votes, delegate and query results on chain',
+    )
+    .setVersion('1.0')
+    .addTag('dApp')
+    .addTag('web3')
+    .build();
+  const document = SwaggerModule.createDocument(_app, config);
+  SwaggerModule.setup('api', _app, document);
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:3000' },
+  });
+
+  setupApiDocs(app);
   await app.listen(3001);
 }
 bootstrap();
