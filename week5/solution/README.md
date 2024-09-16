8c770db2a8176cd796b9503540de0f927afb2483
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-09-16 05:50:20 +0100

    Deploy contracts
    checkin contracts, install project deps and wire up deploy script

---
+++ week5/solution/README.md
@@ -0,0 +1,6 @@
+## How to run
+
+First fill up your `.env` file then, compile by running `npx hardhat compile`
+
+Deploy contracts by running `npx hardhat run scripts/deploy_contracts_with_viem.ts --network sepolia`
+
---
+++ week5/solution/scripts/deploy_contracts_with_viem.ts
@@ -0,0 +1,39 @@
+import { viem } from "hardhat";
+import { toHex, hexToString } from "viem";
+import { sepolia } from "viem/chains";
+import dotenv from "dotenv";
+dotenv.config({ path: '../.env' });
+
+
+async function deployContract(contractName: string, contractDeployArgs: any[]) {
+
+    const publicClient = await viem.getPublicClient();
+    const [deployer, otherAccount] = await viem.getWalletClients({
+        chain: sepolia,
+        account: `0x${process.env.PRIVATE_KEY}` as `0x${string}`,
+    });
+    const contract = await viem.deployContract(contractName, contractDeployArgs);
+    return { publicClient, deployer, otherAccounts: [otherAccount], contract };
+}
+
+async function main() {
+    const tokenName = "LolzToken";
+    const tokenSymbol = "LoL";
+    // deploy lotterToken
+    const { contract: lotteryTokenContract, deployer } = await deployContract("LotteryToken", [tokenName, tokenSymbol]);
+
+    console.log(`Deployed lotteryToken contract to ${lotteryTokenContract.address} by ${deployer.account.address}`);
+
+    const { contract: lotteryContract, deployer: lotteryDeployer } = await deployContract("Lottery", [
+        tokenName,
+        tokenSymbol,
+        10000n, 5n, 1n,
+    ]);
+
+    console.log(`Deployed lottery contract to ${lotteryContract.address} by ${lotteryDeployer.account.address}`);
+}
+
+main().catch(err => {
+    console.error(err);
+    process.exitCode = 1;
+});
